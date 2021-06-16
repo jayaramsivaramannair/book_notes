05304efa71fee3ae5793a6c5286e3426e5be8b68
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-06-16 06:38:21 -0400

    Added a modal which enables a flip of the card for seeing more details

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal.js'
 
 export default function BookCard({ result }) {
+
+    const [showDetails, setShowDetails] = useState(false)
+
+    const hideModal = (detail) => {
+        setShowDetails(detail)
+    }
+
+    const showModal = () => {
+        console.log('buttonClicked')
+        setShowDetails(true)
+    }
 
     if (typeof result === 'undefined') {
         return <p> There is no image to display </p>
@@ -9,23 +21,32 @@ export default function BookCard({ result }) {
 
     return (
         <div className="card">
-            <div className="image">
-                {
-                    (typeof result.volumeInfo.imageLinks !== 'undefined') ?
-                        <img alt={result.volumeInfo.title} src={result.volumeInfo.imageLinks.thumbnail} />
-                        : 'There is no image to display!'
-                }
-            </div>
-            <div className="content">
-                <p>Title: {result.volumeInfo.title}</p>
-                <p>Author: {result.volumeInfo.authors[0]}</p>
-                <p>Publisher: {result.volumeInfo.publisher}</p>
-                <p>Publication Date: {result.volumeInfo.publishedDate}</p>
-                <div className="buttons">
-                    <button className="ui primary">Details</button>
-                    <button className="ui primary">Add to Library</button>
-                </div>
-            </div>
+            {
+                (showDetails) ? <Modal result={result} hideModal={hideModal} /> :
+                    <>
+                        <div className="image">
+                            {
+                                (typeof result.volumeInfo.imageLinks !== 'undefined') ?
+                                    <img alt={result.volumeInfo.title} src={result.volumeInfo.imageLinks.thumbnail} />
+                                    : 'There is no image to display!'
+                            }
+                        </div>
+                        <div className="content">
+                            <p>Title: {result.volumeInfo.title}</p>
+                            <p>Author: {result.volumeInfo.authors[0]}</p>
+                            <p>Publisher: {result.volumeInfo.publisher}</p>
+                            <p>Publication Date: {result.volumeInfo.publishedDate}</p>
+                            <div className="buttons">
+                                <button className="ui primary" onClick={showModal}>Flip for Details</button>
+                                {
+                                    (showDetails) ? <Modal result={result} hideModal={hideModal} /> : ''
+                                }
+                                <button className="ui primary">Add to Library</button>
+                            </div>
+                        </div>
+                    </>
+            }
+
         </div>
     )
 }
